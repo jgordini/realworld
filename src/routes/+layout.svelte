e247fd0f5daa13928dca1c426155c60ad60d386c
Jeremy Gee <92876980+jgordini@users.noreply.github.com>
2023-10-30 12:27:41 -0500

    updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import { navigating } from '$app/stores';
 	import Nav from './Nav.svelte';
 	import PreloadingIndicator from './PreloadingIndicator.svelte';
+    import "../app.css";
 </script>
 
 {#if $navigating}
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
-
-export default {
-	kit: {
-		adapter: adapter({ edge: true })
-	}
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
 };
+export default config;
